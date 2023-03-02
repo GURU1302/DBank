eaabbb86bd215eef19f2c29d33d1c5653359fac3
PIYUSH GHOSH <piyushghosh@GURUs-MacBook-Air.local>
2023-03-02 16:16:35 +0530

    initial commit

---
+++ dist/defibank_frontend/index.js
@@ -0,0 +1,35 @@
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+/******/ 	// The require scope
+/******/ 	var __webpack_require__ = {};
+/******/ 	
+/************************************************************************/
+/******/ 	/* webpack/runtime/make namespace object */
+/******/ 	(() => {
+/******/ 		// define __esModule on exports
+/******/ 		__webpack_require__.r = (exports) => {
+/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 			}
+/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/************************************************************************/
+var __webpack_exports__ = {};
+/*!********************************************!*\
+  !*** ./src/defibank_frontend/src/index.js ***!
+  \********************************************/
+__webpack_require__.r(__webpack_exports__);
+Object(function webpackMissingModule() { var e = new Error("Cannot find module '../../declarations/defibank'"); e.code = 'MODULE_NOT_FOUND'; throw e; }());
+
+
+window.addEventListener("load", function(){
+  // console.log("finished loading");
+  const currentAmount=Object(function webpackMissingModule() { var e = new Error("Cannot find module '../../declarations/defibank'"); e.code = 'MODULE_NOT_FOUND'; throw e; }())();
+  this.document.getElementById("value").innerText=currentAmount;
+})
+
+/******/ })()
+;
+//# sourceMappingURL=index.js.map
